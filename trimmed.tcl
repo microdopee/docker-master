23a8403b20e303b30cb61ee65d63af0b68d32926
microdopee <76610900+microdopee@users.noreply.github.com>
2021-11-10 09:03:28 -0800

    Add files via upload
    with pfb slopes!!!!!!!!!!!!!!!!

---
+++ trimmed.tcl
@@ -0,0 +1,345 @@
+#  This runs the basic default_richards test case.
+#  This run, as written in this input file, should take
+#  3 nonlinear iterations.
+
+#
+# Import the ParFlow TCL package
+#
+lappend auto_path $env(PARFLOW_DIR)/bin 
+package require parflow
+namespace import Parflow::*
+
+pfset FileVersion 4
+
+pfset Process.Topology.P 1
+pfset Process.Topology.Q 1
+pfset Process.Topology.R 1
+
+#---------------------------------------------------------
+# Computational Grid
+#---------------------------------------------------------
+pfset ComputationalGrid.Lower.X                  0.0
+pfset ComputationalGrid.Lower.Y                  0.0
+pfset ComputationalGrid.Lower.Z                  0.0
+
+pfset ComputationalGrid.DX	               2.5
+pfset ComputationalGrid.DY                       2.5
+pfset ComputationalGrid.DZ	               2.5
+
+pfset ComputationalGrid.NX                       99
+pfset ComputationalGrid.NY                       19
+pfset ComputationalGrid.NZ                       14
+
+#---------------------------------------------------------
+# The Names of the GeomInputs
+#---------------------------------------------------------
+pfset GeomInput.Names "block1_input solidinput"
+
+
+#---------------------------------------------------------
+# Domain Geometry Input
+#---------------------------------------------------------
+#pfset GeomInput.domain_input.InputType            Box
+#pfset GeomInput.domain_input.GeomName             domain
+
+#---------------------------------------------------------
+# Domain Geometry
+#---------------------------------------------------------
+#pfset Geom.domain.Lower.X                        0
+#pfset Geom.domain.Lower.Y                        0
+#pfset Geom.domain.Lower.Z                        0
+
+#pfset Geom.domain.Upper.X                        247.5
+#pfset Geom.domain.Upper.Y                        47.5
+#pfset Geom.domain.Upper.Z                        35
+
+
+
+#----------------------------------------------------------------------------
+# Additional block to input solid file
+#----------------------------------------------------------------------------
+pfset GeomInput.solidinput.InputType  SolidFile
+pfset GeomInput.solidinput.GeomNames  sol
+pfset GeomInput.solidinput.FileName   apples.pfsol
+
+pfset Geom.sol.Patches "left right front back bottom top"
+
+#----------------------------------------------------------------------------
+# Block1 Geometry Input
+#----------------------------------------------------------------------------
+pfset GeomInput.block1_input.InputType        Box
+pfset GeomInput.block1_input.GeomName   	block1
+
+#----------------------------------------------------------------------------
+# Block1 Geometry
+#----------------------------------------------------------------------------
+pfset Geom.block1.Lower.X                        -99999999.0
+pfset Geom.block1.Lower.Y                       -99999999.0
+pfset Geom.block1.Lower.Z                        -99999999.0
+
+pfset Geom.block1.Upper.X                      99999999.0
+pfset Geom.block1.Upper.Y                        99999999.0
+pfset Geom.block1.Upper.Z                        99999999.0
+
+#-----------------------------------------------------------------------------
+# Perm
+#-----------------------------------------------------------------------------
+pfset Geom.Perm.Names "sol"
+
+pfset Geom.sol.Perm.Type     Constant
+pfset Geom.sol.Perm.Value    15.0
+
+pfset Perm.TensorType               TensorByGeom
+
+pfset Geom.Perm.TensorByGeom.Names  "block1"
+
+pfset Geom.block1.Perm.TensorValX  1.0
+pfset Geom.block1.Perm.TensorValY  1.0
+pfset Geom.block1.Perm.TensorValZ  1.0
+
+#-----------------------------------------------------------------------------
+# Phases
+#-----------------------------------------------------------------------------
+
+pfset Phase.Names "water"
+
+pfset Phase.water.Density.Type	Constant
+pfset Phase.water.Density.Value	1.0
+
+pfset Phase.water.Viscosity.Type	Constant
+pfset Phase.water.Viscosity.Value	1.0
+
+#-----------------------------------------------------------------------------
+# Gravity
+#-----------------------------------------------------------------------------
+
+pfset Gravity				1.0
+
+#-----------------------------------------------------------------------------
+# Setup timing info
+#-----------------------------------------------------------------------------
+
+pfset TimingInfo.BaseUnit		1.0
+pfset TimingInfo.StartCount		0
+pfset TimingInfo.StartTime		0.0
+pfset TimingInfo.StopTime               365.0
+pfset TimingInfo.DumpInterval	       -10
+pfset TimeStep.Type                     Constant
+pfset TimeStep.Value                    0.1
+
+#-----------------------------------------------------------------------------
+# Porosity
+#-----------------------------------------------------------------------------
+
+pfset Geom.Porosity.GeomNames          sol
+
+pfset Geom.sol.Porosity.Type    Constant
+pfset Geom.sol.Porosity.Value   0.4
+
+#-----------------------------------------------------------------------------
+# Domain
+#-----------------------------------------------------------------------------
+pfset Domain.GeomName sol
+
+#-----------------------------------------------------------------------------
+# Relative Permeability
+#-----------------------------------------------------------------------------
+
+pfset Phase.RelPerm.Type               VanGenuchten
+pfset Phase.RelPerm.GeomNames          sol
+pfset Geom.sol.RelPerm.Alpha        1.5
+pfset Geom.sol.RelPerm.N            2.0    
+
+#---------------------------------------------------------
+# Saturation
+#---------------------------------------------------------
+
+pfset Phase.Saturation.Type            VanGenuchten
+pfset Phase.Saturation.GeomNames       sol
+pfset Geom.sol.Saturation.Alpha     0.5
+pfset Geom.sol.Saturation.N         2.0
+pfset Geom.sol.Saturation.SRes      0.2
+pfset Geom.sol.Saturation.SSat      0.99
+
+#-----------------------------------------------------------------------------
+# Time Cycles
+#-----------------------------------------------------------------------------
+pfset Cycle.Names constant
+pfset Cycle.constant.Names		"alltime"
+pfset Cycle.constant.alltime.Length	 1
+pfset Cycle.constant.Repeat		-1
+
+#-----------------------------------------------------------------------------
+# Boundary Conditions: Pressure
+#-----------------------------------------------------------------------------
+pfset BCPressure.PatchNames "left right front back bottom top"
+
+
+pfset Patch.left.BCPressure.Type			FluxConst
+pfset Patch.left.BCPressure.Cycle			"constant"
+pfset Patch.left.BCPressure.alltime.Value		0.0
+
+pfset Patch.right.BCPressure.Type			FluxConst
+pfset Patch.right.BCPressure.Cycle			"constant"
+pfset Patch.right.BCPressure.RefGeom			sol
+pfset Patch.right.BCPressure.RefPatch			bottom
+pfset Patch.right.BCPressure.alltime.Value		0.0
+
+pfset Patch.front.BCPressure.Type			FluxConst
+pfset Patch.front.BCPressure.Cycle			"constant"
+pfset Patch.front.BCPressure.alltime.Value		0.0
+
+pfset Patch.back.BCPressure.Type			FluxConst
+pfset Patch.back.BCPressure.Cycle			"constant"
+pfset Patch.back.BCPressure.alltime.Value		0.0
+
+pfset Patch.bottom.BCPressure.Type			FluxConst
+pfset Patch.bottom.BCPressure.Cycle			"constant"
+pfset Patch.bottom.BCPressure.alltime.Value		0.0
+
+pfset Patch.top.BCPressure.Type			        FluxConst
+pfset Patch.top.BCPressure.Cycle			"constant"
+pfset Patch.top.BCPressure.alltime.Value		-0.2
+
+
+#---------------------------------------------------------
+# Initial conditions: water pressure
+#---------------------------------------------------------
+
+pfset ICPressure.Type                                   HydroStaticPatch
+pfset ICPressure.GeomNames                              sol
+pfset Geom.sol.ICPressure.Value                      0.0
+pfset Geom.sol.ICPressure.RefGeom                    sol
+pfset Geom.sol.ICPressure.RefPatch                   bottom
+
+#----------------------------------------------------------------------------
+# Contaminants
+#----------------------------------------------------------------------------
+pfset Contaminants.Names ""
+
+#----------------------------------------------------------------------------
+# Wells
+#----------------------------------------------------------------------------
+pfset Wells.Names ""
+
+#-----------------------------------------------------------------------------
+# Phase sources:
+#-----------------------------------------------------------------------------
+
+pfset PhaseSources.water.Type                         Constant
+pfset PhaseSources.water.GeomNames                    block1
+pfset PhaseSources.water.Geom.block1.Value        0.0
+
+
+#-----------------------------------------------------------------------------
+# Exact solution specification for error calculations
+#-----------------------------------------------------------------------------
+
+pfset KnownSolution                                    NoKnownSolution
+
+
+#-----------------------------------------------------------------------------
+# Set solver parameters
+#-----------------------------------------------------------------------------
+pfset Solver                                             Richards
+pfset Solver.MaxIter                                     1000000
+
+pfset Solver.Nonlinear.MaxIter                           20
+pfset Solver.Nonlinear.ResidualTol                       1e-1
+pfset Solver.Nonlinear.EtaChoice                         EtaConstant
+pfset Solver.Nonlinear.EtaValue                          1e-5
+pfset Solver.Nonlinear.UseJacobian                       True
+pfset Solver.Nonlinear.DerivativeEpsilon                 1e-2
+
+pfset Solver.Linear.KrylovDimension                      10
+
+pfset Solver.Linear.Preconditioner                       MGSemi
+pfset Solver.Linear.Preconditioner.MGSemi.MaxIter        1
+pfset Solver.Linear.Preconditioner.MGSemi.MaxLevels      100
+
+pfset Solver.WriteSiloSubsurfData True
+pfset Solver.WriteSiloPressure True
+pfset Solver.WriteSiloSaturation True
+
+#---------------------------------------------------------
+# Topo slopes in x-direction
+#---------------------------------------------------------
+ 
+pfset TopoSlopesX.Type "PFBFile"
+pfset TopoSlopesX.GeomNames "domain"
+pfset TopoSlopesX.FileName pb.slope_x.pfb
+
+ 
+#---------------------------------------------------------
+# Topo slopes in y-direction
+#---------------------------------------------------------
+ 
+pfset TopoSlopesY.Type "PFBFile"
+pfset TopoSlopesY.GeomNames "domain"
+pfset TopoSlopesY.FileName pb.slope_y.pfb
+ 
+#---------------------------------------------------------
+# Mannings coefficient
+#---------------------------------------------------------
+ 
+pfset Mannings.Type "Constant"
+pfset Mannings.GeomNames "sol"
+pfset Mannings.Geom.sol.Value 2.3e-7
+
+#-----------------------------------------------------------------------------
+# Specific Storage
+#-----------------------------------------------------------------------------
+ 
+pfset SpecificStorage.Type            Constant
+pfset SpecificStorage.GeomNames       "sol"
+pfset Geom.sol.SpecificStorage.Value 1.0e-4
+ 
+#-----------------------------------------------------------------------------
+# Run and Unload the ParFlow output files
+#-----------------------------------------------------------------------------
+pfrun rect_sol
+pfundist rect_sol
+
+#-----------------------------------------------------------------------------
+# If running as test; check output.
+# You do not need this for normal PF input files; this is done so the examples
+# are run and checked as part of our testing process.
+#-----------------------------------------------------------------------------
+if { [info exists ::env(PF_TEST) ] } {
+    set TEST rect_sol
+    source pftest.tcl
+    set sig_digits 4
+
+    set passed 1
+
+    #
+    # Tests 
+    #
+    if ![pftestFile $TEST.out.press.00001.pfb "Max difference in Pressure" $sig_digits] {
+	set passed 0
+    }
+
+    if ![pftestFile $TEST.out.perm_x.pfb "Max difference in perm_x" $sig_digits] {
+	set passed 0
+    }
+    if ![pftestFile $TEST.out.perm_y.pfb "Max difference in perm_y" $sig_digits] {
+	set passed 0
+    }
+    if ![pftestFile $TEST.out.perm_z.pfb "Max difference in perm_z" $sig_digits] {
+	set passed 0
+    }
+
+    if ![pftestFile $TEST.out.specific_storage.pfb "Max difference in specific storage" $sig_digits] {
+	set passed 0
+    }
+
+    if ![pftestFile $TEST.out.porosity.pfb "Max difference in porosity" $sig_digits] {
+	set passed 0
+    }
+
+    if $passed {
+	puts "$TEST : PASSED"
+    } {
+	puts "$TEST : FAILED"
+    }
+}
